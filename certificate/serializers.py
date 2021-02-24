2ab0d7feabee3e197f0143944adbac184b2463e9
Kaushik <kaushikhareesh@gmail.com>
2021-02-24 11:17:43 +0530

    m

--- certificate/serializers.py
+++ certificate/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from.models import *
 
 class CertificateSerializer(serializers.ModelSerializer):
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     class Meta:
-        model=Patient
-        fields=('user','csp','level','certname','certid','certified_date','expiry_date','pdf_url','validity')
+        model=Certificates
+        fields=('user','csp','level','certname','certid','certified_date','expiry_date','pdf_url')
--- certificate/views.py
+++ certificate/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
 from .upload import pdf_upload
 from rest_framework import generics, views, permissions ,response, status
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+from .serializers import *
 
 class CertificateView(views.APIView):
 	permission_classes = [permissions.IsAuthenticated,]
 	http_method_names=['post']
+	parser_classes = (MultiPartParser, FormParser, JSONParser)
 	def post(self, request, *args, **kwargs): 
-		try:
-			pdf=request.data.get('pdf')
-			user=self.request.user
+		# try:
+			user=self.request.user	
+			request.data["user"]=user.id
+			pdf=request.FILES['pdf']
 			pdf_name=user.email+request.data.get("certname")
 			request.data["pdf_url"]='https://storage.googleapis.com/certificate_pdf/'+pdf_name
 			pdf_upload(pdf,pdf_name)
 			serializer = CertificateSerializer(data=request.data)
-			if serializer.is_valid:
+			if serializer.is_valid():
+				print(serializer.data)
 				serializer.save()
+			print(serializer.data)
 			return response.Response(serializer.data,status=status.HTTP_200_OK)
-		except Exception as e:
-			return response.Response(str(e))
+		# except Exception as e:
+		# 	return response.Response(str(e))
