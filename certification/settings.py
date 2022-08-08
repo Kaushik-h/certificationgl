42b31b39fc2ea6324b589074f7b29eb7295b4f95
kaushik <43905951+Kaushik-h@users.noreply.github.com>
2022-08-08 11:12:24 +0530

    Update settings.py

--- certification/settings.py
+++ certification/settings.py
@@ -155,4 +155,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kaushikhareesh@gmail.com'
-EMAIL_HOST_PASSWORD = 'djqhyyatogitknxv'
+EMAIL_HOST_PASSWORD = 'sample'
